7654e732f203e75bf5ecdf635795792aa7bb8331
Alexandre Miguel <aleronupe@gmail.com>
2021-04-18 13:56:28 -0300

    Comment prints and reactivate menu

--- serv_central/src/main.c
+++ serv_central/src/main.c
@@ -18,7 +18,7 @@ void mata_threads() {
     printf("Matou o processo\n");
     servStruct.flag_run = 0;
     sleep(1);
-    //  desliga_telas();
+    desliga_telas();
     // fecha_conexoes_TCP();
     fecha_cliente();
     exit(0);
@@ -52,13 +52,13 @@ int main(int argc, const char *argv[]) {
     servStruct.sensorAbrt5 = 0;
     servStruct.sensorAbrt6 = 0;
 
-    // iniciaTela();
+    iniciaTela();
     monta_cliente();
 
-    // pthread_t menu_tid;
+    pthread_t menu_tid;
     // pthread_t server_tid;
 
-    // pthread_create(&menu_tid, NULL, (void *)carregaMenu, (void *)&servStruct);
+    pthread_create(&menu_tid, NULL, (void *)carregaMenu, (void *)&servStruct);
     // pthread_create(&server_tid, NULL, (void *)monta_servidor, (void *)&servStruct);
     
     while (servStruct.flag_run == 1) {
@@ -66,7 +66,7 @@ int main(int argc, const char *argv[]) {
         requisita_temperatura(&servStruct);
     }
 
-    // pthread_join(menu_tid, NULL);
+    pthread_join(menu_tid, NULL);
     // pthread_join(server_tid, NULL);
 
     mata_threads();
